aff5caa66ebff8d61bf04bdb65a5ddd0ff9db691
Carlos Pérez Gutiérrez <AlgusDark@MacBook-Air-de-Carlos.local>
2015-05-11 00:54:56 -0700

    First week up

---
+++ public/app/partials/weeks/01/files/app.js
@@ -0,0 +1,13 @@
+// File: ./app.js
+var restify = require('restify');
+
+var server = restify.createServer();
+
+server.get(/.*/, restify.serveStatic({
+    'directory' : './public/',
+    'default' : 'index.html'
+}));
+
+server.listen(8124, function(){
+    console.log('Listening from 8124');
+});
---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+var app = angular.module('app', ['ui.bootstrap', 'angular-loading-bar', 'ngRoute', 'hljs']);
+
+app.controller('mainController', function($scope) {
+
+    // Dropdown ======================
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+});
+
+app.config(function($routeProvider, $locationProvider, hljsServiceProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'public/app/partials/index.html'
+        })
+        .when('/week1', {
+            templateUrl: 'public/app/partials/weeks/01/index.html'
+        })
+        .when('/stack', {
+            templateUrl: 'public/app/partials/stack.html'
+        });
+
+    hljsServiceProvider.setOptions({
+        // replace tab with 4 spaces
+        tabReplace: '    '
+    });
+});
